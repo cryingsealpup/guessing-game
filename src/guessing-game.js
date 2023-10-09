005dc59d38dd2bd76055cf584a57b6d4ca018f60
radchenkoed <radchenkoed@sgu.ru>
2023-10-10 03:08:00 +0400

    finished task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max) {
+        this.min = 0
+        this.max = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        return Math.round((this.max  - (this.max - this.min) / 2))
     }
 
     lower() {
-
+        this.setRange(this.min, this.guess())
     }
 
     greater() {
-
+        this.setRange(this.guess(), this.max)
     }
 }
 
